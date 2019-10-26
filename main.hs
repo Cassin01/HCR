1a3a522c92440b8d899856dd16e674247a9331c4
Cassin01 <koyojb@gmail.com>
2019-10-26 13:44:59 +0900

    first commit

---
+++ main.hs
@@ -0,0 +1,19 @@
+main = do
+    r <- readLn -- 入力が一行に一文字の場合
+    -- [a, b] <- map read . words <$> getLine -- 一行に入力が複数ある場合
+    -- putStrLn $ show $ solve a b -- showの返り値がString型の場合
+    -- putStrLn $ solve a b
+
+    putStrLn $ show $ solve r -- showの返り値がString型の場合
+
+solve :: Int -> Int
+solve r = 3 * r ^ 2
+
+
+{-- その他 備忘録
+    {{{ 型変換
+       int 2 float: fromIntegral
+       float 2 int: round
+       int 2 string: show
+    }}}
+--}
