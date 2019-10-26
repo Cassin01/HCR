bfa76f17fceba5614f3490d453d830c763b1d087
Cassin01 <koyojb@gmail.com>
2019-10-26 14:47:25 +0900

    float 2 int

--- main.hs
+++ main.hs
@@ -2,8 +2,8 @@ main :: IO ()
 main = do
     -- {{{ 入力
 
-    r <- readLn -- 入力が一行に一文字の場合
-    -- [a, b] <- map read . words <$> getLine -- 一行に入力が複数ある場合
+    -- r <- readLn -- 入力が一行に一文字の場合
+    as <- map read . words <$> getLine -- 一行に入力が複数ある場合
 
     -- }}}
 
@@ -14,16 +14,24 @@ main = do
 
     -- }}}
 
-    putStrLn $ show $ solve r -- showの返り値がString型の場合
+    putStrLn $ show $ solve as -- showの返り値がString型の場合
 
-solve :: Int -> Int
-solve r = 3 * r ^ 2
+solve :: [Int] -> Int
+solve [a, b, c] = if floor (fromIntegral b / fromIntegral a) < c
+                    then floor (fromIntegral b / fromIntegral a)
+                    else c
 
 
 {-- その他 備忘録
     {{{ 型変換
        int 2 float: fromIntegral
-       float 2 int: round
+       float 2 int: round 四捨五入
+                    floor 床関数
+                    ceiling 天井関数
        int 2 string: show
+    }}}
+
+    {{{ モジュール
+        Data.List: sort
     }}}
 --}
